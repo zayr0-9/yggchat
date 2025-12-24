093708b32e0678c25008d423f0193b9331d80d4a
zayr0-9 <z3yr0.0.9@gmail.com>
2025-12-24 02:51:29 +0000

    sidebar smooth

--- ygg-chat/client/ygg-chat-r/src/containers/sideBar.tsx
+++ ygg-chat/client/ygg-chat-r/src/containers/sideBar.tsx
@@ -115,7 +115,7 @@ const SideBar: React.FC<SideBarProps> = ({ limit = 8, className = '', projects =
 
   return (
     <aside
-      className={`relative z-10 ${isWeb ? 'h-[100vh]' : 'h-full'} bg-transparent dark:bg-transparent shadow-lg rounded-r-xl border-r border-neutral-200 dark:border-neutral-700 flex flex-col transition-all duration-300 ease-in-out flex-shrink-0 ${isCollapsed ? 'w-16 acrylic-subtle' : 'w-64 md:w-72 lg:w-80 xl:w-90 acrylic-light'} ${className}`}
+      className={`relative z-10 ${isWeb ? 'h-[100vh]' : 'h-full'}  shadow-lg rounded-r-xl border-r border-neutral-200 dark:border-neutral-700 flex flex-col transition-all duration-300 ease-in-out backdrop-blur-sm bg-neutral-100/50 dark:bg-transparent flex-shrink-0 ${isCollapsed ? 'w-16 ' : 'w-64 md:w-72 lg:w-80 xl:w-90 '} ${className}`}
       aria-label='Recent conversations'
     >
       {/* Toggle Button */}
